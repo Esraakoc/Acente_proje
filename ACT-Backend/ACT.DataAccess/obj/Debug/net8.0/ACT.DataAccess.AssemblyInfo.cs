c233822eeb1df5ec7e76c0d6fc6db7cfc8d5b52d
Esra Koç <esrakc239@gmail.com>
2025-02-04 22:32:09 +0300

    update

--- ACT-Backend/ACT.DataAccess/obj/Debug/net8.0/ACT.DataAccess.AssemblyInfo.cs
+++ ACT-Backend/ACT.DataAccess/obj/Debug/net8.0/ACT.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ACT.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c4a36bc2a32de61d5c2b055794f1e15c45c18ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb65ad56bb055a7e108bb41bc9590ba10535d6f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ACT.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ACT.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
